f3cfaab306ec0fbc3c8dadea7329333521f9a1b8
Peter Barry <pete.barry@gmail.com>
2017-07-26 19:17:58 +0100

    Added checks for values close to zero

--- Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -65,14 +65,9 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     
   // MatrixXd Hj = tools.CalculateJacobian(z);
     
-    rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+    rho = sqrt(x_(0) * x_(0) + x_(1) * x_(1));
     phi = atan2(x_(1), x_(0));
-    
-    if (fabs(rho) < 0.001) {
-        rho_dot = 0;
-    } else {
-        rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
-    }
+    rho_dot = (x_(0) * x_(2) + x_(1) * x_(3)) / rho;
         
     
     VectorXd z_pred(3);
