982403e0689700784a2723f47605c95de20e58a1
Peter Barry <pete.barry@gmail.com>
2017-08-13 17:04:48 +0100

    Init checkin of project, including updated init code

---
+++ Unscented-Kalman-Filters/project/Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -0,0 +1,160 @@
+#include "ukf.h"
+#include "Eigen/Dense"
+#include <iostream>
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+/**
+ * Initializes Unscented Kalman filter
+ */
+UKF::UKF() {
+
+  ///* initially set to false, set to true in first call of ProcessMeasurement
+  is_initialized_ = false;
+
+  // if this is false, laser measurements will be ignored (except during init)
+  use_laser_ = true;
+
+  // if this is false, radar measurements will be ignored (except during init)
+  use_radar_ = true;
+
+  ///* State dimension
+  n_x_ = 5;
+
+  ///* Augmented state dimension
+  n_aug_ = 7;
+
+  // initial state vector
+  x_ = VectorXd(n_x_);
+
+  // initial covariance matrix
+  P_ = MatrixXd(n_x_, n_x_
+    // Start P_ with identity  matric.
+
+  P_ <<     1,    0,    0,    0,    0,
+            0,    1,    0,    0,    0,
+            0,    0,    1,    0,    0,
+            0,    0,    0,    1,    0,
+            0,    0,    0,    0,    1;
+
+
+  ///* predicted sigma points matrix
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
+  //create sigma point matrix TODO: Needed ? reused from class.
+  Xsig_aug_ = MatrixXd(n_aug, 2 * n_aug + 1);
+
+  ///* time when the state is true, in us
+  time_us_ = 0;
+
+
+  ///* Sigma point spreading parameter
+  double lambda_= = 3 - n_aug_;;
+
+  ///* Weights of sigma points - reused from class.
+  weights_ = VectorXd(2*n_aug_+1);
+  double weight_0 = lambda_/(lambda_+n_aug_);
+  weights(0) = weight_0;
+  for (int i=1; i<2*n_aug_+1; i++) {
+    double weight = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight;
+  }
+
+  // Process noise standard deviation longitudinal acceleration in m/s^2
+
+  // too high for a bike std_a_ = 30;
+  std_a_ = 0.3; //TODO: tune this value.
+
+  // Process noise standard deviation yaw acceleration in rad/s^2
+  // too high for a bike std_yawdd_ = 30;
+  std_yawdd_ = 0.6; //TODO: Tune this value.
+
+  // Laser measurement noise standard deviation position1 in m
+  std_laspx_ = 0.15;
+
+  // Laser measurement noise standard deviation position2 in m
+  std_laspy_ = 0.15;
+
+  // Radar measurement noise standard deviation radius in m
+  std_radr_ = 0.3;
+
+  // Radar measurement noise standard deviation angle in rad
+  std_radphi_ = 0.03;
+
+  // Radar measurement noise standard deviation radius change in m/s
+  std_radrd_ = 0.3;
+
+
+  NIS_radar_ = 0;
+  NIS_lidar_ = 0;
+
+  /**
+  TODO:
+
+  Complete the initialization. See ukf.h for other member properties.
+
+  Hint: one or more values initialized above might be wildly off...
+  */
+}
+
+UKF::~UKF() {}
+
+/**
+ * @param {MeasurementPackage} meas_package The latest measurement data of
+ * either radar or laser.
+ */
+void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
+  /**
+  TODO:
+
+  Complete this function! Make sure you switch between lidar and radar
+  measurements.
+  */
+}
+
+/**
+ * Predicts sigma points, the state, and the state covariance matrix.
+ * @param {double} delta_t the change in time (in seconds) between the last
+ * measurement and this one.
+ */
+void UKF::Prediction(double delta_t) {
+  /**
+  TODO:
+
+  Complete this function! Estimate the object's location. Modify the state
+  vector, x_. Predict sigma points, the state, and the state covariance matrix.
+  */
+}
+
+/**
+ * Updates the state and the state covariance matrix using a laser measurement.
+ * @param {MeasurementPackage} meas_package
+ */
+void UKF::UpdateLidar(MeasurementPackage meas_package) {
+  /**
+  TODO:
+
+  Complete this function! Use lidar data to update the belief about the object's
+  position. Modify the state vector, x_, and covariance, P_.
+
+  You'll also need to calculate the lidar NIS.
+  */
+}
+
+/**
+ * Updates the state and the state covariance matrix using a radar measurement.
+ * @param {MeasurementPackage} meas_package
+ */
+void UKF::UpdateRadar(MeasurementPackage meas_package) {
+  /**
+  TODO:
+
+  Complete this function! Use radar data to update the belief about the object's
+  position. Modify the state vector, x_, and covariance, P_.
+
+  You'll also need to calculate the radar NIS.
+  */
+}
